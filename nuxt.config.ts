a22f13822a0962dc3b113c2b7aace7f504eef33c
Alexandre Switalski <alexandre.swit@gmail.com>
2019-10-09 16:06:34 +0200

    chore: add frontmatter-markdown to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,33 @@
-import { BlogRaw } from "./types";
+import NuxtConfiguration from "@nuxt/config";
+import { Configuration as WebpackConfiguration } from "webpack";
+
+import { ArticleRaw } from "./types";
 
 const { fr, en } = require("./blog/blogs.json");
 import { head } from "./config/head";
 
 export default {
+  server: {
+    port: 3003
+  },
   env: {},
   head,
   loading: { color: "#3B8070" },
   css: ["~/assets/css/main.css"],
-  build: {},
+  build: {
+    extend(config: WebpackConfiguration) {
+      config.module!.rules.push({
+        test: /\.md$/,
+        loader: "frontmatter-markdown-loader"
+      });
+    }
+  },
   buildModules: ["@nuxt/typescript-build"],
   modules: ["@nuxtjs/axios"],
   axios: {},
   generate: {
     routes: ["404"]
-      .concat(fr.map((w: BlogRaw) => `/blog/${w.slug}`))
-      .concat(en.map((w: BlogRaw) => `/blog/en/${w.slug}`))
+      .concat(fr.map((w: ArticleRaw) => `/blog/${w.slug}`))
+      .concat(en.map((w: ArticleRaw) => `/blog/en/${w.slug}`))
   }
 };
